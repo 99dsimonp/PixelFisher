fad50c24fa3956eac1ed98a2b75f67a692dc1dd6
Simon <mokio12@yahoo.dk>
2021-05-03 13:42:21 +0200

    Cleanup

--- fiskerman.py
+++ fiskerman.py
@@ -1,16 +1,12 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
-#from PIL import Image
 import mss
 import pyautogui
 import time
-#import sounddevice as sd
 import queue
 import pyaudio
 import struct
 import math
-import wave
 import random
 
 sct = mss.mss()
@@ -32,7 +28,7 @@ Threshold = 90
 #assert(p.get_default_input_device_info()['name'] == 'CABLE Output (VB-Audio Virtual ')
 print(p.get_default_input_device_info()['name'])
 print(p.get_default_output_device_info()['name'])
-
+#FIND INDEX OF YOUR VIRTUAL DEVICE AND INPUT IN FOLLOWING STATEMENT
 stream = p.open(format = FORMAT,
                 channels = CHANNELS,
                 rate = RATE,
@@ -60,17 +56,13 @@ while True:
     pyautogui.press('1')
     random_time = random.random()
     time.sleep(2.7 + random_time)
-    #scr = sct.shot(mon=2, output='fullscreen-wow.jpg')
     scr2 = sct.grab({"mon": 2, "top": 0,"left": 0, "width": 2560, "height": 1440})
     img = np.array(scr2)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    #img2 = cv.imread('fullscreen-wow.jpg', 0)
 
-    #cv.imshow('output', img)
     if cv.waitKey(25) & 0xFF == ord("x"):
         break
 
-    #img2 = img.copy()
     template = cv.imread('bobber.jpg', 0)
     w, h = template.shape[::-1]
     methods = ['cv.TM_CCOEFF_NORMED']#, 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
@@ -79,11 +71,8 @@ while True:
         # Apply template Matching
         res = cv.matchTemplate(img,template,method)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-        if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-            top_left = min_loc
-        else:
-            top_left = max_loc
+        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum, else take max
+        top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
         mid = (top_left[0] + w/2, top_left[1] + h/2)
         pyautogui.moveTo(mid)
